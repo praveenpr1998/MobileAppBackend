99e52676eb3a34b286706608ab4a380652ac346f
praveenpr1998 <48721940+praveenpr1998@users.noreply.github.com>
2020-04-17 10:42:56 +0530

    Update auth.js

--- api/policies/auth.js
+++ api/policies/auth.js
@@ -3,9 +3,6 @@ var jwt = require('jsonwebtoken');
 module.exports=function(req,res,next){
    
     try{
-        
-
-        
         jwt.verify(req.body.token,sails.config.secret.key);
         next();
     }
